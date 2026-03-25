d3c21cf041d32168546e30d55ab453394952edfb
Dimon2075 <dmitriy.gusakov.2006@mail.ru>
2026-03-25 14:57:00 +0300

    Исправление ошибок

--- Bank/BankAccount.cs
+++ Bank/BankAccount.cs
@@ -92,7 +92,7 @@ namespace BankAccountNS
             BankAccount ba = new BankAccount("Mr.Roman Abramovich", 11.99);
             ba.Credit(5.77);
             ba.Debit(11.22);
-            Console.WriteLine("Current balance is ${ 0}", ba.Balance);
+            Console.WriteLine("Current balance is ${0}", ba.Balance);
             Console.ReadLine();
         }
     }
